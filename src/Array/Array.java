a37764b1ff114456ea997451745ba3fd6d421d2f
Borisov-Ilya <borisov.ilya22@gmail.com>
2022-11-21 15:57:01 +0300

    Задача №3_Массивы

---
+++ src/Array/Array.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        int temp = nums[0];
+
+        nums[0] = nums[nums.length - 1];
+        nums[nums.length - 1] = temp;
+        int sum = nums[0] + nums[2];
+        System.out.println(sum);
+    }
+}
